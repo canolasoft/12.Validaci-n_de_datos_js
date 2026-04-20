4d302926d94859acbd55dc757aa53a8130371f8e
Fernando Walpert <walllpapert@gmail.com>
2026-04-20 10:10:35 -0300

    cambios
    validación desde el servidor

--- Controlador/registro_proceso.php
+++ Controlador/registro_proceso.php
@@ -3,7 +3,20 @@
 $nombre = $_POST['nombre'];
 $email = $_POST["email"];
 $pass = $_POST["pass"];
+$pass2 = $_POST["pass2"];
 
+if ($nombre == "" || $email == "" || $pass == "") {
+    echo "Completa todos los datos";
+    return;
+} else if (strlen($pass) < 8) {
+    echo "La contraseña debe tener al menos 8 caracteres";
+    return;
+} else if ($pass != $pass2) {
+    echo "Las contraseñas no coinciden";
+    return;
+}
+
+echo "Validación de datos correcta: <br>";
 echo "El nombre ingresado es: " . $nombre . "<br>";
 echo "El email ingresado es: " . $email . "<br>";
 echo "La contraseña ingresada es: " . $pass . "<br>";
